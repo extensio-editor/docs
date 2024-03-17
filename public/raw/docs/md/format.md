c610c111ef2044ffe361eca5b399cd74aa8b0c49
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-17 22:18:04 +0100

    Created format.md and loading documents

---
+++ public/raw/docs/md/format.md
@@ -0,0 +1,43 @@
+# Markdown format
+
+Our version of markdown essentially works the same.
+However there are some slight diffrences that are explained here.
+
+## Headings
+
+Headings using the equal and minus signs are not a thing.
+
+## Text styling
+
+Underscores can not be used for **bold** text or *italic* text.
+
+## Block quotes (Block quotes)
+
+Block quotes are replaced with notes, essentially the same just a diffrent name.
+
+The first line will automatically be considered the note's title, and amphasised as in the example.
+
+Line breaks will also be added automatically in notes.
+
+Nested block quotes are also not a thing.
+
+> Example note
+> This is what a note would look like.
+
+## Lists
+
+Ordered lists are made using the numbers.
+
+Unordered lists with dashes.
+
+Lists can **not** be nested.
+
+## Links
+
+You can not add titles to links.
+
+Links are also not able to be made using the triangular brackets.
+
+## Everything else
+
+Everything that is not listed in this file works the same as it would normally.
--- src/views/DocView.vue
+++ src/views/DocView.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>{{ docData }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
 export default defineComponent({
   name: "DocView",
-  beforeRouteEnter(to) {
-    console.log(`attempting to load doc for ${to.params.name}`);
+  data() {
+    return {
+      docName: "",
+      docData: "",
+    };
+  },
+  beforeRouteEnter(
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) {
+    next((vm) => {
+      console.log(`attempting to load doc for ${to.params.name}`);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (vm as any).docName = to.params.name || "";
+
+      fetch(`/raw/${to.params.name}.md`)
+        .then((result) => {
+          const data = result.text();
+
+          return data;
+        })
+        .then((data) => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (vm as any).docData = data;
+        });
+    });
   },
 });
 </script>
